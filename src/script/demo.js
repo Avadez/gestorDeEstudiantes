04b0027556310b6a01393f01ff5bd743d8e7e959
Avadez <converzeronimo@gmail.com>
2024-06-22 19:57:29 -0400

    creacion de sistema de demos

---
+++ src/script/demo.js
@@ -0,0 +1,15 @@
+// creando alumnos de ejemplo
+gesEst.creator(alumnos, "Carlos", "Andrés", "Ramírez", "Vargas");
+gesEst.creator(alumnos, "Isabella", "Fernanda", "Silva", "López");
+gesEst.creator(alumnos, "Diego", "Alejandro", "Gómez", "Pérez");
+gesEst.creator(alumnos, "Valentina", "Camila", "Hernández", "Torres");
+gesEst.creator(alumnos, "Mateo", "Sebastián", "Rojas", "Jiménez");
+gesEst.creator(alumnos, "Daniela", "Sofía", "Ortega", "García");
+gesEst.creator(alumnos, "Javier", "Andrés", "Vega", "Mendoza");
+gesEst.creator(alumnos, "Natalia", "María", "Castro", "Ruiz");
+
+// terminando de crear alumnos de ejemplo
+
+// añadiendoles historial de notas
+
+// terminando de añadirles historial de notas
--- src/script/script.js
+++ src/script/script.js
@@ -26,18 +26,6 @@ console.log(alumnos);
 //console.log(alumnos);
 */
 
-// creando alumnos de ejemplo
-gesEst.creator(alumnos, "Carlos", "Andrés", "Ramírez", "Vargas");
-gesEst.creator(alumnos, "Isabella", "Fernanda", "Silva", "López");
-gesEst.creator(alumnos, "Diego", "Alejandro", "Gómez", "Pérez");
-gesEst.creator(alumnos, "Valentina", "Camila", "Hernández", "Torres");
-gesEst.creator(alumnos, "Mateo", "Sebastián", "Rojas", "Jiménez");
-gesEst.creator(alumnos, "Daniela", "Sofía", "Ortega", "García");
-gesEst.creator(alumnos, "Javier", "Andrés", "Vega", "Mendoza");
-gesEst.creator(alumnos, "Natalia", "María", "Castro", "Ruiz");
-
-// terminando de crear alumnos de ejemplo
-
 // Declaro las funciones que ocupo en el codigo
 
 let estadoDeDespliegue = "no";
