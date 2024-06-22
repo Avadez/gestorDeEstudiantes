266a24d663579132ede86b873f0f2e494202fc79
Avadez <converzeronimo@gmail.com>
2024-06-21 22:49:02 -0400

    se añaden ejemplos de alumnos completamente ficticios para pruebas futuras

--- src/script/script.js
+++ src/script/script.js
@@ -25,6 +25,19 @@ gesEst.promediar(alumnos);
 console.log(alumnos);
 //console.log(alumnos);
 */
+
+// creando alumnos de ejemplo
+gesEst.creator(alumnos, "Carlos", "Andrés", "Ramírez", "Vargas");
+gesEst.creator(alumnos, "Isabella", "Fernanda", "Silva", "López");
+gesEst.creator(alumnos, "Diego", "Alejandro", "Gómez", "Pérez");
+gesEst.creator(alumnos, "Valentina", "Camila", "Hernández", "Torres");
+gesEst.creator(alumnos, "Mateo", "Sebastián", "Rojas", "Jiménez");
+gesEst.creator(alumnos, "Daniela", "Sofía", "Ortega", "García");
+gesEst.creator(alumnos, "Javier", "Andrés", "Vega", "Mendoza");
+gesEst.creator(alumnos, "Natalia", "María", "Castro", "Ruiz");
+
+// terminando de crear alumnos de ejemplo
+
 let estadoDeDespliegue = "no";
 const crearNuevoUsuario = `
     <!--se solicita el nombre-->
